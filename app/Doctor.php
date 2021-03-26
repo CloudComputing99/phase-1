18332156e22100b94628e4f8aff1ce2770144565
Hazhir <kaa.hazhir@gmail.com>
2021-03-26 22:03:38 +0430

    model classes has been created with migrations now we made essential params and functions to models

--- app/Doctor.php
+++ app/Doctor.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Doctor extends Model
 {
-    //
+    public function visits(){
+        return $this->hasMany(Visit::class);
+    }
+
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
+
+    public function favorites(){
+        return $this->hasMany(Visit::class);
+    }
 }
